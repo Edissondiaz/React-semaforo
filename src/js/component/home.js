70b4ca3581adf233a7c7300cced8e770243f9273
Edissondiaz <edissondiaz123@gmail.com>
2021-10-19 00:02:25 +0000

    guardado

---
+++ src/js/component/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Semaforo } from "./semaforo";
+
+//include images into your bundle
+
+//create your first component
+export function Home() {
+	return <Semaforo />;
+}
